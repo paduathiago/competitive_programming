0db1e52a2934b676ed3cc1f27a8ad003942fbda7
Thiago Padua <paduathiago@outlook.com>
2025-04-08 20:33:29 -0300

    add final version of in braille

--- in_braille_1357.cpp
+++ in_braille_1357.cpp
@@ -34,10 +34,8 @@ int main() {
     };
 
     unsigned digits = 101;
-    while(digits > 0)
+    while(std::cin >> digits && digits > 0)
     {
-        std::cin >> digits;
-
         char sOrB;
         std::cin >> sOrB;
 
@@ -66,36 +64,36 @@ int main() {
             }
         }
 
-         else if(sOrB == 'B')
-         {
-             std::string reading;
-             std::vector<string> brailleDigits;
-             for (int i = 0; i < 3; ++i)
-             {
-                 for (int j = 0; j < digits; ++j)
-                 {
-                     if (i == 0)
-                     {
-                          std::cin >> reading;
-                          brailleDigits.pushback(reading);
-                     }
-                     else if (i == 1)
-                     {
-                         std::cin >> reading;
-                         brailleDigits[j] += reading;
-                     }
-                     else
-                     {
-                         std::cin >> reading;
-                     }
-                 }
-             }
-             
-             for(size_t i = 0; i < brailleDigits.size; ++i)
-             {
-                 std::cout << brailleToDigit[brailleDigits[i]];
-             }
-             std::cout << std::endl;
+        else if(sOrB == 'B')
+        {
+            std::string reading;
+            std::vector<std::string> brailleDigits;
+            for (int i = 0; i < 3; ++i)
+            {
+                for (unsigned j = 0; j < digits; ++j)
+                {
+                    if (i == 0)
+                    {
+                        std::cin >> reading;
+                        brailleDigits.push_back(reading);
+                    }
+                    else if (i == 1)
+                    {
+                        std::cin >> reading;
+                        brailleDigits[j] += reading;
+                    }
+                    else
+                    {
+                        std::cin >> reading;
+                    }
+                }
+            }
+
+            for(size_t i = 0; i < brailleDigits.size(); ++i)
+            {
+                std::cout << brailleToDigit[brailleDigits[i]];
+            }
+            std::cout << std::endl;
         }
     }
 
