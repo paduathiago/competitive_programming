de08bb3da94972de060c6d8fd3701243e4ccdbf6
Thiago Padua <paduathiago@outlook.com>
2025-04-07 23:39:36 -0300

    add in Braile - implement digit to braille

---
+++ in_braille_1357.cpp
@@ -0,0 +1,83 @@
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+
+int main() {
+
+    std::unordered_map<std::string, char> brailleToDigit = {
+        {"*...", '1'},
+        {"*.*.", '2'},
+        {"**..", '3'},
+        {"**.*", '4'},
+        {"*..*", '5'},
+        {"***.", '6'},
+        {"****", '7'},
+        {"*.**", '8'},
+        {".**.", '9'},
+        {".***", '0'}
+    };
+
+    std::unordered_map<char, std::vector<std::string>> digitToBraille = {
+        {'1', {"*.", "..", ".."}},
+        {'2', {"*.", "*.", ".."}},
+        {'3', {"**", "..", ".."}},
+        {'4', {"**", ".*", ".."}},
+        {'5', {"*.", ".*", ".."}},
+        {'6', {"**", "*.", ".."}},
+        {'7', {"**", "**", ".."}},
+        {'8', {"*.", "**", ".."}},
+        {'9', {".*", "*.", ".."}},
+        {'0', {".*", "**", ".."}}
+    };
+
+    unsigned digits = 101;
+    while(digits > 0)
+    {
+        std::cin >> digits;
+
+        char sOrB;
+        std::cin >> sOrB;
+
+        if(sOrB == 'S')
+        {
+            std::string number;
+            std::cin >> number;
+
+            std::vector<std::vector<std::string>> brailleFromNumbers;
+
+            for(const char& digit : number)
+            {
+                brailleFromNumbers.push_back(digitToBraille[digit]);
+            }
+            for (int i = 0; i < 3; ++i)
+            {
+                for (std::size_t j = 0; j < brailleFromNumbers.size(); ++j)
+                {
+                    std::cout << brailleFromNumbers[j][i];
+                    if (j != brailleFromNumbers.size() - 1) // Check if it's not the last element
+                    {
+                        std::cout << " ";
+                    }
+                }
+                std::cout << std::endl;
+            }
+        }
+
+        // else if(sOrB == 'B')
+        // {
+        //     std::vector<std::vector<char>> brailleDigits;
+        //     for (int i = 0; i < 2; ++i)
+        //     {
+        //         for (int j = 0; j < digits; ++j)
+        //         {
+                    
+        //         }
+        //     }
+        // }
+    }
+
+    return 0;
+}
